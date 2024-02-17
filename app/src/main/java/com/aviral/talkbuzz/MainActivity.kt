cdbfb4a1088b3bb17a7f224f9498b1ac343cf66a
Aviral Kaushik <zaviralkaushik@gmail.com>
2024-02-17 13:48:48 +0530

    Updated To-Dos

--- app/src/main/java/com/aviral/talkbuzz/MainActivity.kt
+++ app/src/main/java/com/aviral/talkbuzz/MainActivity.kt
@@ -11,8 +11,17 @@ class MainActivity : AppCompatActivity() {
     /**
      * TODO:-
      *      1.] Splash Screen Implementation.
-     *      2.] Welcome E-mail Implementation.
+     *      2.] Welcome E-mail Implementation. Library: https://github.com/yesidlazaro/GmailBackground
      *      3.] Repository logic implementation in each and every view model possible.
+     *              ViewModels                  Status
+     *          1.] Login View Model            Done
+     *          2.] Register View Model         Done
+     *          3.] Channel View Model          Not Yet
+     *      4.] Do the setting in stream dashboard so that app doesn't crash any how.
+     *      5.] Do testing of the application.
+     *      6.] Generate a good looking Readme.MD file.
+     *      7.] Add TalkBuzz to the pins project in the github.
+     *      8.] Add TalkBuzz as the project (Dec - Mar) and do post for this project.
      * */
 
     private lateinit var binding : ActivityMainBinding
